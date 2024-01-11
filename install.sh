eb57c42c3607c1e8a446b94290339bf73debf8c4
Jacob <jahuffine@proton.me>
2024-01-11 03:42:45 +0000

    install script error fixed

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#Asking for initsetup.sh
-read -r -p "Do you want to run initial setup first? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    bash initsetup.sh
-fi
-
 #Set Var
 user="$(whoami)"
 current_path=$(pwd)
 
+#Asking for initsetup.sh#
+read -r -p "Do you want to run initial setup first? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	bash inisetup.sh
+fi
+
 #Create Scripts Folder
-mkdir /home/$user/hypr/scripts
+mkdir /home/$user/.config/hypr/scripts
 
 #Removing original hyperland config and creating symlink to new
 rm /home/$user/.config/hypr/hyprland.conf
@@ -20,7 +20,5 @@ ln -s $current_path/hypr/hyprland.conf /home/$user/.config/hypr/hyprland.conf
 
 #setting Wallpaper
 mkdir /home/$user/.config/hypr/wallpaper
-cp $current_path/wallpapers/evening-landscape.jpg /home/$user/hypr/wallpaper
-ln -s $current_path/scripts/wallpaper.sh /home/$user/hypr/scripts/wallpaper.sh
-
-echo "Install Complete"
+cp $current_path/wallpapers/evening-landscape.jpg /home/$user/.config/hypr/wallpaper
+ln -s $current_path/scripts/wallpaper.sh /home/$user/.config/hypr/scripts/wallpaper.sh
